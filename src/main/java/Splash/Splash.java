f41702b9591e7336fae4850ddc9e39bfcb789c7c
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-06-12 10:52:14 -0700

    new project

---
+++ src/main/java/Splash/Splash.java
@@ -0,0 +1,15 @@
+package Splash;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+
+    void Splash(){
+        setSize(1170, 650);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
